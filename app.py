576e80ddea674071200fbf5d04d8b20d73e245b7
hackhub817 <piyushguptaji123@gmail.com>
2024-10-31 23:03:20 +0530

    change

--- app.py
+++ app.py
@@ -376,4 +376,4 @@ def convert_to_markdown(data):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False,use_reloader=False, host='0.0.0.0', port=port)
